814b50dd95de39521235f1da182f5920a4e18225
lwath125 <loris.wathelet@ulb.be>
2026-09-13 14:00:23 +0200

    Second push

--- modules/rigid_motion/frame.cpp
+++ modules/rigid_motion/frame.cpp
@@ -15,7 +15,7 @@ namespace ais4104::rigid_motion {
 Eigen::Matrix3d rotate_x(double radians)
 {
     Eigen::Matrix3d r =  Eigen::Matrix3d::Zero();
-    r << 1.0, 0.0, 0.0,0.0,0.0, std::cos(radians),-std::sin(radians),0.0,std::sin(radians),std::cos(radians);// << for 3x3 matrix
+    r << 1.0, 0.0, 0.0, 0.0, std::cos(radians),-std::sin(radians),0.0,std::sin(radians),std::cos(radians);// << for 3x3 matrix
     return r;
 }
 
@@ -33,7 +33,7 @@ Eigen::Matrix3d rotate_y(double radians)
 Eigen::Matrix3d rotate_z(double radians)
 {
     Eigen::Matrix3d r =  Eigen::Matrix3d::Zero();
-    r << std::cos(radians), -std::sin(radians), 0.0, std::sin(radians), std::cos(radians), 0.0, 0.0, 1.0;
+    r << std::cos(radians), -std::sin(radians), 0.0, std::sin(radians), std::cos(radians), 0.0, 0.0, 0.0, 1.0;
     return r;
 }
 
@@ -45,9 +45,7 @@ Eigen::Matrix3d rotation_matrix_from_frame_axes(const Eigen::Vector3d &x, const 
     r.col(0) = x;
     r.col(1) = y;
     r.col(2) = z;
-
     return r;
-
 }
 
 //TASK: 1h
@@ -55,7 +53,7 @@ Eigen::Matrix3d rotation_matrix_from_frame_axes(const Eigen::Vector3d &x, const 
 Eigen::Matrix3d rotation_matrix_from_axis_angle(const Eigen::Vector3d &axis, double radians)
 {
     Eigen::Matrix3d m;
-    m = Eigen::Matrix3d::Identity() +  std::sin(radians) * skew_symmetric(axis) + (1-std::sin(radians)) * skew_symmetric(axis) * skew_symmetric(axis);
+    m = Eigen::Matrix3d::Identity() +  std::sin(radians) * skew_symmetric(axis) + (1-std::cos(radians)) * skew_symmetric(axis) * skew_symmetric(axis);
     return m;
 }
 
@@ -73,7 +71,7 @@ Eigen::Matrix3d rotation_matrix_from_euler(const Eigen::Vector3d &e, praxis::axi
     }
 
     const std::array<std::uint8_t, 3> axes = praxis::axis_indices(order);
-    return rotation_matrix_from_axis_angle(Eigen::Vector3d::Unit(axes[0]), e[0]) * 
+    return rotation_matrix_from_axis_angle(Eigen::Vector3d::Unit(axes[0]), e[0]) *
            rotation_matrix_from_axis_angle(Eigen::Vector3d::Unit(axes[1]), e[1]) *
            rotation_matrix_from_axis_angle(Eigen::Vector3d::Unit(axes[2]), e[2]);
 }
@@ -87,7 +85,7 @@ Eigen::Vector3d euler_from_rotation_matrix(const Eigen::Matrix3d &r, praxis::axi
         Eigen::Vector3d e = Eigen::Vector3d::Zero();
 
         e[0] = atan2(r(1,0), r(0,0));
-        e[1] = atan2(-r(1,0),sqrt(r(0,0) * r(0,0) + r(0,1) * r(0,1)));
+        e[1] = atan2(-r(2,0),sqrt(r(0,0) * r(0,0) + r(1,0) * r(1,0)));
         e[2] = atan2(r(2,1), r(2,2));
 
         return e; //not sure
@@ -99,14 +97,14 @@ Eigen::Vector3d euler_from_rotation_matrix(const Eigen::Matrix3d &r, praxis::axi
 }
 
 //TASK: 2b
-//REFERENCE: 
+//REFERENCE:
 Eigen::Matrix3d rotation_matrix_from_transform(const Eigen::Matrix4d &tf)
 {
     return Eigen::Matrix3d::Zero();
 }
 
 //TASK: 2c
-//REFERENCE: 
+//REFERENCE:
 Eigen::Matrix4d transformation_matrix_from_rotation_position(const Eigen::Matrix3d &r, const Eigen::Vector3d &p)
 {
     //Eigen:: Matrix4d t = Eigen::Matrix4d::Identity();
@@ -117,7 +115,7 @@ Eigen::Matrix4d transformation_matrix_from_rotation_position(const Eigen::Matrix
 }
 
 //TASK: 2d
-//REFERENCE: 
+//REFERENCE:
 Eigen::Matrix4d transformation_matrix_from_position(const Eigen::Vector3d &p)
 {
     //Eigen:: Matrix4d t = Eigen::Matrix4d::Identity();
@@ -127,14 +125,14 @@ Eigen::Matrix4d transformation_matrix_from_position(const Eigen::Vector3d &p)
 }
 
 //TASK: 2e
-//REFERENCE: 
+//REFERENCE:
 Eigen::Matrix4d transformation_matrix_from_rotation(const Eigen::Matrix3d &r)
 {
     return Eigen::Matrix4d::Zero();
 }
 
 //TASK: 2f
-//REFERENCE: 
+//REFERENCE:
 Eigen::Matrix4d inverse(const Eigen::Matrix4d &tf)
 {
     return Eigen::Matrix4d::Zero();
